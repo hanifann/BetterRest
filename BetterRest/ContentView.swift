67ec55da1cc7631676c7fe558347adf8843c111b
hanifan <hanifannurulhaq@outlook.com>
2024-03-09 20:37:36 +0700

    add stepper for daily coffe intake, add navigationTitle, add toolbar

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,18 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var coffeAmount = 1
     
     @State private var wakeUp = Date.now
     
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours" ,value: $sleepAmount, in: 4...12, step: 0.15)
-            
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-                .labelsHidden()
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12)
+                
+                Text("Daily coffe intake")
+                    .font(.headline)
+                Stepper("\(coffeAmount) cup(s)", value: $coffeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
         }
-        .padding()
     }
+}
+
+func calculateBedTime() {
+    
 }
 
 #Preview {
