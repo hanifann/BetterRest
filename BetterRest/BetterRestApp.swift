7fa032325832e529afd5372cf79ea1bb18a01edf
hanifan <hanifannurulhaq@outlook.com>
2024-03-09 20:03:53 +0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by hanifan nurul haq on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
